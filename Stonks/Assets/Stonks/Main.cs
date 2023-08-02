29a6ac746d6a69e5d45d25d57af9341c7befd22d
R4ngle <111386276+R4ngle@users.noreply.github.com>
2023-08-01 21:01:53 -0400

    Adv
    Added functionality for investing in Adv.
    Allows player to invest in Adv and sets their advInt to the corresponding dice via random.range

--- Stonks/Assets/Stonks/Main.cs
+++ Stonks/Assets/Stonks/Main.cs
@@ -6,12 +6,18 @@ using UnityEngine.UI;
 //using Random = System.Random;
 public class Main : MonoBehaviour
 {
-    public string GetText;
-    public string GetText2;
-    public string GetText3;
+    public string GetDevText1;
+    public string GetDevText2;
+    public string GetDevText3;
+    public string GetAdvText1;
+    public string GetAdvText2;
+    public string GetAdvText3;
     public GameObject inputField;
     public GameObject inputField3;
     public GameObject inputField2;
+    public GameObject inputField4;
+    public GameObject inputField5;
+    public GameObject inputField6;
     public List<string> NumbersList;
     public int PlayerScore;
     public int StartingMoney = 1000;
@@ -27,17 +33,22 @@ public class Main : MonoBehaviour
     public int UnitPrice1 = 150;
     public int UnitPrice2 = 150;
     public int UnitPrice3 = 150;
-    public int InputInt1;
-    public int InputInt2;
-    public int InputInt3;
+    public int InputDevInt1;
+    public int InputDevInt2;
+    public int InputDevInt3;
+    public int InputAdvInt4;
+    public int InputAdvInt5;
+    public int InputAdvInt6;
 
-    IEnumerable<int> AdvDice;
+    /*Attempted use of Ienumerables, decided to just declare ints with rangdom.range within the Adv class
+     * IEnumerable<int> AdvDice;
     IEnumerable<int> AdvDice1 = Enumerable.Range(1, 15);
     IEnumerable<int> AdvDice2 = Enumerable.Range(1, 12);
     IEnumerable<int> AdvDice3 = Enumerable.Range(1, 10);
     IEnumerable<int> AdvDice4 = Enumerable.Range(1, 8);
     IEnumerable<int> AdvDice5 = Enumerable.Range(1, 6);
     IEnumerable<int> AdvDice6 = Enumerable.Range(1, 4);
+    */
     public List<string> Prod1 = new List<string>()
     {
         "1","2"
@@ -61,78 +72,78 @@ public class Main : MonoBehaviour
     }
     public void getDevInput()
     {
-        GetText = inputField.GetComponent<Text>().text;
-        GetText2 = inputField2.GetComponent<Text>().text;
-        GetText3 = inputField3.GetComponent<Text>().text;
+        GetDevText1 = inputField.GetComponent<Text>().text;
+        GetDevText2 = inputField2.GetComponent<Text>().text;
+        GetDevText3 = inputField3.GetComponent<Text>().text;
     }
     public void DevInvest()
     {
-        int.TryParse(GetText, out InputInt1);
-        int.TryParse(GetText2, out InputInt2);
-        int.TryParse(GetText3, out InputInt3);
+        int.TryParse(GetDevText1, out InputDevInt1);
+        int.TryParse(GetDevText2, out InputDevInt2);
+        int.TryParse(GetDevText3, out InputDevInt3);
     }
     public void CategoryInvestmentDev()
     {
        //CEO 1
-        if (InputInt1 == 150)
+        if (InputDevInt1 == 150)
         {
             UnitPrice1 = 215;
         }
-        else if (InputInt1 == 250)
+        else if (InputDevInt1 == 250)
         {
             UnitPrice1 = 275;
         }
-        else if (InputInt1 == 400)
+        else if (InputDevInt1 == 400)
         {
             UnitPrice1 = 350;
         }
-        else if (InputInt1 == 550)
+        else if (InputDevInt1 == 550)
         {
             UnitPrice1 = 400;
         }
-        else if (InputInt1 == 850)
+        else if (InputDevInt1 == 850)
         {
             UnitPrice1 = 700;
         }
         //CEO 2
-        if(InputInt2 == 150)
+        if(InputDevInt2 == 150)
         {
             UnitPrice2 = 215;
         }
-        else if (InputInt2 == 250)
+        else if (InputDevInt2 == 250)
         {
             UnitPrice2 = 275;
         }
-        else if (InputInt2 == 400)
+        else if (InputDevInt2 == 400)
         {
             UnitPrice2 = 350;
         }
-        else if (InputInt2 == 550)
+        else if (InputDevInt2 == 550)
         {
             UnitPrice2 = 400;
         }
-        else if (InputInt2 == 850)
+        else if (InputDevInt2 == 850)
         {
             UnitPrice2 = 700;
         }
         //CEO 3
-        if(InputInt2 == 150)
+        if(InputDevInt2 == 150)
         {
             UnitPrice2 = 215;
         }
-        else if (InputInt2 == 250)
+        else if (InputDevInt2 == 250)
         {
             UnitPrice1 = 275;
         }
-        else if (InputInt2 == 400)
+        else if (InputDevInt2 == 400)
         {
             UnitPrice1 = 350;
         }
-        else if (InputInt2 == 550)
+        else if (InputDevInt2 == 550)
         {
             UnitPrice2 = 400;
         }
-        else if (InputInt1 == 850)
+        else if (InputDevInt2 == 850)
         {
             UnitPrice2 = 700;
         }
@@ -186,8 +197,92 @@ public class Main : MonoBehaviour
             Prod3.AddRange(new List<string> { "3,4,5" });
         }
     }
+    public void GetAdvInput()
+    {
+        GetAdvText1 = inputField4.GetComponent<Text>().text;
+        GetAdvText2 = inputField5.GetComponent<Text>().text;
+        GetAdvText3 = inputField6.GetComponent<Text>().text;
+    }
+    public void GetDevInvest()
+    {
+        int.TryParse(GetAdvText1, out InputAdvInt4);
+        int.TryParse(GetAdvText2, out InputAdvInt5);
+        int.TryParse(GetAdvText3, out InputAdvInt6);
+    }
     public void CategoryInvestmentAdv()
     {
-       /
+        int AdvDice1 = Random.Range(1, 15);
+        int AdvDice2 = Random.Range(1, 12);
+        int AdvDice3 = Random.Range(1, 10);
+        int AdvDice4 = Random.Range(1, 8);
+        int AdvDice5 = Random.Range(1, 6);
+        int AdvDice6 = Random.Range(1, 4);
+        if(InputAdvInt4 == 0)
+        {
+            Adv = AdvDice1;
+        }else if(InputAdvInt4 == 200)
+        {
+            Adv = AdvDice2;
+        }else if(InputAdvInt4 == 300)
+        {
+            Adv = AdvDice3;
+        }else if( InputAdvInt4 == 400)
+        {
+            Adv = AdvDice4;
+        }else if(InputAdvInt4 == 550)
+        {
+            InputAdvInt4 = AdvDice5;
+        }else if(InputAdvInt4 == 850)
+        {
+            InputAdvInt4 = AdvDice6;
+        }
+        if (InputAdvInt5 == 0)
+        {
+            Adv2 = AdvDice1;
+        }
+        else if (InputAdvInt5 == 200)
+        {
+            Adv2 = AdvDice2;
+        }
+        else if (InputAdvInt5 == 300)
+        {
+            Adv2 = AdvDice3;
+        }
+        else if (InputAdvInt5 == 400)
+        {
+            Adv2 = AdvDice4;
+        }
+        else if (InputAdvInt5 == 550)
+        {
+            Adv2 = AdvDice5;
+        }
+        else if (InputAdvInt5 == 850)
+        {
+            Adv = AdvDice6;
+        }
+        if (InputAdvInt6 == 0)
+        {
+            Adv3 = AdvDice1;
+        }
+        else if (InputAdvInt6 == 200)
+        {
+            Adv3 = AdvDice2;
+        }
+        else if (InputAdvInt6 == 300)
+        {
+            Adv3 = AdvDice3;
+        }
+        else if (InputAdvInt6 == 400)
+        {
+            Adv3 = AdvDice4;
+        }
+        else if (InputAdvInt6 == 550)
+        {
+            Adv3 = AdvDice5;
+        }
+        else if (InputAdvInt6 == 850)
+        {
+            Adv3 = AdvDice6;
+        }
     }
 }
